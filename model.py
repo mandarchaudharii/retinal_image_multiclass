7b51f044c6d654862008e5a5cc63edeffce8250c
Mandar Chaudhari <118111153+mandarchaudharii@users.noreply.github.com>
2024-10-18 09:58:56 +0530

    Update model.py

--- model.py
+++ model.py
@@ -3,5 +3,5 @@ from transformers import ViTForImageClassification
 
 def load_model():
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    model = ViTForImageClassification.from_pretrained('vit_retinal_classification').to(device)
+    model = ViTForImageClassification.from_pretrained('mandarchaudharii/retinal_multiclass').to(device)
     return model, device
