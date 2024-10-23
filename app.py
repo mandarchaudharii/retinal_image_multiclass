979a4b8ec903ebc0fc758f7edccf428eed6f9db5
Mandar Chaudhari <118111153+mandarchaudharii@users.noreply.github.com>
2024-10-24 01:14:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,15 +12,63 @@ import torchvision.transforms.v2 as transforms
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-# Label Full Form Dict
+#Label Full Form Dict
 labels_dict = {
-    # ... (same as before)
+    'ID': 'ID',
+    'Disease_Risk': 'Disease_Risk',
+    'DR': 'Diabetic retinopathy',
+    'ARMD': 'Age-related macular degeneration',
+    'MH': 'Media haze',
+    'DN': 'Drusen',
+    'MYA': 'Myopia',
+    'BRVO': 'Branch retinal vein occlusion',
+    'TSLN': 'Tessellation',
+    'ERM': 'Epiretinal membrane',
+    'LS': 'Laser scars',
+    'MS': 'Macular scars',
+    'CSR': 'Central serous retinopathy',
+    'ODC': 'Optic disc cupping',
+    'CRVO': 'Central retinal vein occlusion',
+    'TV': 'Tortuous vessels',
+    'AH': 'Asteroid hyalosis',
+    'ODP': 'Optic disc pallor',
+    'ODE': 'Optic disc edema',
+    'ST': 'Optociliary shunt',
+    'AION': 'Anterior ischemic optic neuropathy',
+    'PT': 'Parafoveal telangiectasia',
+    'RT': 'Retinal traction',
+    'RS': 'Retinitis',
+    'CRS': 'Chorioretinitis',
+    'EDN': 'Exudation',
+    'RPEC': 'Retinal pigment epithelium changes',
+    'MHL': 'Macular hole',
+    'RP': 'Retinitis pigmentosa',
+    'CWS': 'Cotton-wool spots',
+    'CB': 'Coloboma',
+    'ODPM': 'Optic disc pit maculopathy',
+    'PRH': 'Preretinal hemorrhage',
+    'MNF': 'Myelinated nerve fibers',
+    'HR': 'Hemorrhagic retinopathy',
+    'CRAO': 'Central retinal artery occlusion',
+    'TD': 'Tilted disc',
+    'CME': 'Cystoid macular edema',
+    'PTCR': 'Post-traumatic choroidal rupture',
+    'CF': 'Choroidal folds',
+    'VH': 'Vitreous hemorrhage',
+    'MCA': 'Macroaneurysm',
+    'VS': 'Vasculitis',
+    'BRAO': 'Branch retinal artery occlusion',
+    'PLQ': 'Plaque',
+    'HPED': 'Hemorrhagic pigment epithelial detachment',
+    'CL': 'Collateral'
 }
 
 # Load labels
-labels_list = [
-    # ... (same as before)
-]
+labels_list = ['ID', 'Disease_Risk', 'DR', 'ARMD', 'MH', 'DN', 'MYA', 'BRVO', 'TSLN',
+               'ERM', 'LS', 'MS', 'CSR', 'ODC', 'CRVO', 'TV', 'AH', 'ODP', 'ODE', 'ST',
+               'AION', 'PT', 'RT', 'RS', 'CRS', 'EDN', 'RPEC', 'MHL', 'RP', 'CWS',
+               'CB', 'ODPM', 'PRH', 'MNF', 'HR', 'CRAO', 'TD', 'CME', 'PTCR', 'CF',
+               'VH', 'MCA', 'VS', 'BRAO', 'PLQ', 'HPED', 'CL']
 
 PRET_MEANS = [0.485, 0.456, 0.406]
 PRET_STDS = [0.229, 0.224, 0.225]
